03f96ccc6c8736cf1d367e08e7246322bed2d9d5
abbyxianggu <abbyxianggu@gmail.com>
2026-05-21 00:15:49 +1000

    navbar stuff + pages

--- Enterprise Computing Project/KKINVSYS/page.py
+++ Enterprise Computing Project/KKINVSYS/page.py
@@ -4,3 +4,19 @@ page = Blueprint("page", __name__)
 @page.route('/')
 def home():
     return render_template ('index.html')
+
+@page.route('/inventory')
+def inv():
+    return render_template ('inventory.html')
+
+@page.route('/sales')
+def sales():
+    return render_template ('sales.html')
+
+@page.route('/alerts')
+def alerts():
+    return render_template ('alerts.html')
+
+@page.route('/reports')
+def reports():
+    return render_template ('reports.html')
