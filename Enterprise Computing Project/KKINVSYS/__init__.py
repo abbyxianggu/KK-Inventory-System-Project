6201db3091700231f209ebc625b35a6ce63e80cc
abbyxianggu <abbyxianggu@gmail.com>
2026-05-20 14:25:40 +1000

    Add files via upload

---
+++ Enterprise Computing Project/KKINVSYS/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'lololol'
+    
+    from .page import page
+    
+    app.register_blueprint(page, url_prefix = '/')
+
+    
+    return app
---
+++ Enterprise Computing Project/KKINVSYS/page.py
@@ -0,0 +1,6 @@
+from flask import Blueprint, render_template
+page = Blueprint("page", __name__)
+
+@page.route('/')
+def home():
+    return render_template ('index.html')
