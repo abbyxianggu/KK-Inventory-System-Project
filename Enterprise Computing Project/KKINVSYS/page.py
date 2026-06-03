f3d13955a95f63adcc749eeb2901ad5cbb0e701f
abbyxianggu <abbyxianggu@gmail.com>
2026-06-03 10:53:52 +1000

    page

--- Enterprise Computing Project/KKINVSYS/page.py
+++ Enterprise Computing Project/KKINVSYS/page.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request
-from datetime import date
-from . import db
-from .database import Donut, Sale
+from datetime import date, timedelta
+from .import db
+from .database import Donut, Sale, Ingredient, RestockRequest
 
 page = Blueprint('page', __name__)
 
@@ -9,14 +9,16 @@ page = Blueprint('page', __name__)
 def index():
     store = request.args.get('store', 'Sydney CBD')
     today = date.today()
+    yesterday = today - timedelta(days=1)
 
     donuts = Donut.query.filter_by(store=store).all()
     total_stock = sum(d.stock for d in donuts)
 
     today_sales = Sale.query.filter_by(store=store, date=today).all()
-    sold_today = sum(s.quantity for s in today_sales)
+    max_possible = sum(d.max_stock for d in donuts)
+    sold_today = max_possible - total_stock
     revenue_today = sum(s.revenue for s in today_sales)
-    low_alerts = len([d for d in donuts if d.stock / d.max_stock < 0.50])
+    low_alerts = len([d for d in donuts if d.stock / d.max_stock < 0.10])
 
     hourly = {}
     for s in today_sales:
@@ -36,6 +38,50 @@ def index():
         low_alerts=low_alerts,
         hourly_labels=hourly_labels,
         hourly_data=hourly_data,
-        alerts=alerts
+        alerts=alerts)
+    
+@page.route('/inventory')
+def inventory():
+    store = request.args.get('store', 'Sydney CBD')
+    donuts = Donut.query.filter_by(store=store).all()
+    ingredients = Ingredient.query.filter_by(store=store).all()
+    
+    return render_template('inventory.html',
+        active='inventory',
+        store=store,
+        donuts=donuts,
+        ingredients=ingredients
     )
     
+@page.route('/sales')
+def sales():
+    store = request.args.get('store', 'Sydney CBD')
+    today = date.today()
+    yesterday = today - timedelta(days=1)
+    
+    donuts = Donut.query.filter_by(store=store).all()
+    sales = Sale.query.filter_by(store=store, date=yesterday).order_by(Sale.hour).all()
+    
+    # 7 day trend
+    seven_days = []
+    for i in range(7):
+        d = yesterday - timedelta(days=6 - i)
+        day_sales = Sale.query.filter_by(store=store, date=d).all()
+        seven_days.append(sum(s.revenue for s in day_sales))
+    
+    trend_labels = [(yesterday - timedelta(days=6 - i)).strftime('%a') for i in range(7)]
+    
+    return render_template('sales.html',
+        active='sales',
+        store=store,
+        donuts=donuts,
+        sales=sales,
+        seven_days=seven_days,
+        trend_labels=trend_labels
+    )
+
+@page.route('/alerts')
+def alerts():
+    store = request.args.get('store', 'Sydney CBD')
+    donuts = Donut.query.filter_by(store=store).all()
+    low = [d for d in donuts if d.stock / d.max_stock < 0.10]
